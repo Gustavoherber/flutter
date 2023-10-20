8a27c99b31e9f2981147d358563de564e443be57
Gustavo Morais <gu.herber@gmail.com>
2023-10-19 22:02:51 -0300

    app imc atualizado

--- app_imc/app_imc/lib/main.dart
+++ app_imc/app_imc/lib/main.dart
@@ -7,8 +7,19 @@ void main() {
   ));
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _alturaController = TextEditingController();
+  final _pesoController = TextEditingController();
+  double margem = 30;
+  String _mensagem = 'Informe sua altura e peso';
+  String _imc = '';
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +27,107 @@ class Home extends StatelessWidget {
       backgroundColor: Colors.amber[100],
       appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text('Calculadora IMC'),
+        title: const Text('Calculadora IMC'),
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Center(
-                child: Container(
-                  child: Image(image: AssetImage('assets/fatguy.png')),
+          child: Padding(
+            padding: EdgeInsets.all(margem),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('assets/fatguy.png'),
+                      width: 200,
+                      height: 200,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      _mensagem,
+                    )
+                  ],
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(margem),
+                      child: const Text(
+                        'Informe',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _alturaController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: 'Altura',
+                          suffixText: 'm',
+                          labelStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: margem,
+                      height: margem,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        controller: _pesoController,
+                        decoration: const InputDecoration(
+                          labelText: 'Peso',
+                          suffixText: 'Kg',
+                          labelStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: margem),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                      ),
+                      child: const Text('Calcular'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// _clickCalculator() {
+//   String pesoTxt = _pesoController.text;
+//   String alturaTxt = _alturaController.text;
+
+//   alturaTxt = alturaTxt.replaceAll(',', '.');
+//   pesoTxt = pesoTxt.replaceAll(',', '.');
+// }
